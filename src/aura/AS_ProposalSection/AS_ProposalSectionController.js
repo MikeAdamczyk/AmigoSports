b3efe32a6684631f3359695aa06a05d0bdc2088a
MikeAdamczyk <hello@codeman.it>
2021-03-08 17:28:09 +0100

    amend popular products section

--- src/aura/AS_ProposalSection/AS_ProposalSectionController.js
+++ src/aura/AS_ProposalSection/AS_ProposalSectionController.js
@@ -1,8 +1,17 @@
-/**
- * Created by BRITENET on 08.03.2021.
- */
 ({
     doInit : function(component, event, helper) {
+
+        let action = component.get("c.getPromotedProducts");
+
+        action.setCallback(this, function (response) {
+            if (component.isValid() && response.getState() === 'SUCCESS') {
+                component.set("v.apparel", response.getReturnValue());
+            } else {
+                console.log('ERROR >>>>> ' + response.error);
+            }
+        });
+        $A.enqueueAction(action);
+
 
         var urlRun = $A.get('$Resource.run');
         var urlCrossfit = $A.get('$Resource.crossfit');
@@ -11,6 +20,6 @@
         component.set('v.backgroundImageURLrun', urlRun);
         component.set('v.backgroundImageURLcrossfit', urlCrossfit);
         component.set('v.backgroundImageURLbike', urlBike);
-    }
 
+    }
 })
