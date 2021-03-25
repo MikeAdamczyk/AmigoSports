4145e672a609dd71e4b1576a6bccb3def4503c01
MikeAdamczyk <hello@codeman.it>
2021-03-25 21:38:55 +0100

    amend order items list

--- src/aura/AS_OrderList/AS_OrderListHelper.js
+++ src/aura/AS_OrderList/AS_OrderListHelper.js
@@ -1,15 +1,16 @@
 ({
     doInit : function(component, event, helper) {
-        console.log('ENTER ORDER LIST INIT.......');
-
         let action = component.get("c.getClosedOrders");
-
         action.setCallback(this, function (response) {
             if (component.isValid() && response.getState() === 'SUCCESS') {
-                console.log('CLOSED ORDERS >>>> ' + response.getReturnValue());
                 component.set("v.orders", response.getReturnValue());
             } else {
-                console.log('ERROR >>>>> ' + response.error);
+                let errors = response.getError();
+                if (errors) {
+                   if (errors[0] && errors[0].message) {
+                      console.log("Error message: " + errors[0].message);
+                   }
+                }
             }
         });
         $A.enqueueAction(action);
---
+++ src/aura/AS_OrderTile/AS_OrderTileController.js
@@ -0,0 +1,9 @@
+({
+    init: function(component, event, helper){
+          helper.init(component, event, helper);
+    },
+
+    check: function(component, event, helper){
+          helper.check(component, event, helper);
+    },
+})
